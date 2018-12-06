7f5508ab31e57cb7e84a8612e2a0b59caf182f14
alan7816 <alan7816@gmail.com>
2018-12-06 14:14:29 +0800

    2

--- 1.py
+++ 1.py
@@ -1,7 +1,10 @@
-import random
+import random #導入random模組
+#產生IP變數
 section1 = random.randint(1,255)
 section2 = random.randint(1,255)
 section3 = random.randint(1,255)
 section4 = random.randint(1,255)
+#將變數組合成字串
 random_ip = str(section1) + ' . ' + str(section2) + ' . ' + str(section3) + ' . ' + str(section4)
+#顯示結果
 print(random_ip)
