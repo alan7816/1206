03c0bf5c57f479e4973434b57c4076ab6302b3a0
alan7816 <alan7816@gmail.com>
2018-12-10 15:45:14 +0800

    1210-1

--- 4.py
+++ 4.py
@@ -5,10 +5,10 @@ mac =  '7    78e3.b5ba.bcb2    DYNAMIC     Gi2/0/5'
 import re
 report = re.match('(\d+)\s+(\w{1,4}.\w{1,4}.\w{1,4})\s+(\w+)\s+(.*)' ,mac).groups()
 
-id = 'VLAN ID ：%s' %(report[0])
-mac = 'MAC ADD：%s' %(report[1])
-Type = 'Type：%s' %(report[2])
-Interface = 'Interface：%s' %(report[3])
+id = '%-10s：%s' %('VLAN ID',report[0])
+mac = '%-10s：%s' %('MAC ADD',report[1])
+Type = '%-10s：%s' %('Type',report[2])
+Interface = '%-10s：%s' %('Interface',report[3])
 print(id)
 print(mac)
 print(Type)
